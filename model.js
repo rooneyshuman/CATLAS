589a79d6d4be161fa954020343914a44d47542ac
rooneyshuman <rooneyshuman@users.noreply.github.com>
2019-05-22 00:54:19 -0700

    Created cat info schema

---
+++ model.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+/**
+ * Define MongoDB schema 
+ */
+const cat = new mongoose.Schema({
+    color : String,
+    dateFound : {
+      type: Date,
+      default : Date.now
+    },
+    location : {
+      long: Number,
+      lat: Number,
+    },
+    breed : String,
+    size : String
+  });
+  
+module.exports = mongoose.model('Cat', cat);
