d0ac00fa500d27cd2bb795c7557261ffc35170d3
rooneyshuman <rooneyshuman@users.noreply.github.com>
2019-05-22 04:22:29 -0700

    Server passes in cats on map GET, routes form, creates new cat on POST

--- routes/index.js
+++ routes/index.js
@@ -3,56 +3,52 @@ var router = express.Router();
 var catList = require('../model');
 var api_key = require('../config').api_key;
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+/* GET home page */
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'CATLAS' });
 });
 
-/* GET about page. */
-router.get('/about', function(req, res, next) {
+/* GET about page */
+router.get('/about', function (req, res, next) {
   res.render('about', { title: 'CATLAS' });
 });
 
-/* GET map page. */
-router.get('/map', function(req, res, next) {
-  res.render('map', { title: 'CATLAS', key: api_key });
+/* GET map page */
+router.get('/map', function (req, res, next) {
+  // Get cats from database
+  catList.find(function (err, cats) {
+    if (err)
+      res.send(err)
+
+    console.log(cats)
+    res.render('map', { title: 'CATLAS', key: api_key, cats: cats });
+  });
 });
 
-/* GET form page. */
-router.get('/form', function(req, res, next) {
+/* GET form page */
+router.get('/form', function (req, res, next) {
   res.render('form', { title: 'CATLAS' });
 });
 
-/* GET/POST for DB. */
-router.get('/cats', function(req, res) {
 
-  // Get cats from database
-  catList.find(function(err, cats) {
-      if (err)
-          res.send(err)
-
-      res.json(cats); // return cats in JSON format
-  });
-});
-
-// Add new cat and send back updated cat list
-router.post('/cats', function(req, res) {
-
+/* POST to database */
+router.post('/add', function (req, res) {
   // Create a new cat
   catList.create({
-      text : req.body.text,
-      done : false
-  }, function(err, todo) {
-      if (err)
-          res.send(err);
-
-      // Get all cats from database (including just added)
-      catList.find(function(err, cats) {
-          if (err)
-              res.send(err)
-          res.json(cats);
-      });
+    color: req.body.color,
+    dateFound: req.body.dateFound,
+    location: {
+      long: req.body.long,
+      lat: req.body.lat
+    },
+    breed: req.body.breed,
+    size: req.body.size,
+    done: false
+  }, function (err, cat) {
+    if (err)
+      res.send(err);
   });
+  res.render('thanks', { title: 'CATLAS' });
 });
 
 module.exports = router;
